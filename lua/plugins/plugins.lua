863cd990eb6797c88566cfa24813d5c3e2376ba7
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2024-06-25 08:05:55 -0400

    feat: update theme to properly use mocha

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -93,16 +93,10 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'catppuccin-mocha'
       require("catppuccin").setup({
-        color_overrides = {
-          mocha = {
-            base = "#000000",
-            mantle = "#000000",
-            crust = "#000000",
-          },
-        }
+        flavour = "mocha"
       })
+      vim.cmd.colorscheme "catppuccin"
     end,
   },
 

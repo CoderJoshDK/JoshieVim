89f51956abdb037a1965f48e6227d7c5c52007e0
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2025-07-30 11:16:34 -0400

    chore: update conform config version

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -7,27 +7,22 @@ return {
       -- Customize or remove this keymap to your liking
       "<leader>f",
       function()
-        require("conform").format({ async = true, lsp_fallback = true })
+        require("conform").format({ async = true, lsp_format = "fallback" })
       end,
       mode = "",
       desc = "Format buffer",
     },
   },
   -- Everything in opts will be passed to setup()
+  ---@module "conform"
+  ---@type conform.setupOpts
   opts = {
     -- Full list with :help conform-formatters
     -- Define your formatters
-    formatters = {
-      ruff_isort = {
-        command = "ruff",
-        args = { "check", "--select", "I", "--fix", "--stdin-filename", "$FILENAME" },
-        stdin = true,
-      },
-    },
     formatters_by_ft = {
       -- lua = { "stylua" },
       json = { "jq", "prettierd", "prettier", stop_after_first = true },
-      python = { "ruff_isort", "ruff_format" },
+      python = { "ruff_organize_imports", "ruff_format" },
       javascript = { "prettierd", "prettier", stop_after_first = true },
       typescript = { "prettierd", "prettier", stop_after_first = true },
       typescriptreact = { "prettierd", "prettier", stop_after_first = true },
@@ -35,7 +30,7 @@ return {
       rust = { "rustfmt", lsp_format = "fallback", args = { "+nightly" } },
     },
     -- Set up format-on-save
-    format_on_save = { timeout_ms = 1000, lsp_fallback = true }, -- Customize formatters
+    format_on_save = { timeout_ms = 1000, lsp_format = "fallback" }, -- Customize formatters
   },
   init = function()
     -- If you want the formatexpr, here is the place to set it

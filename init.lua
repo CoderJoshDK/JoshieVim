307f6be9f07d6146823e0926c68470bdf1327496
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2025-10-25 11:37:58 -0400

    refactor: lazy setup based on docs

--- init.lua
+++ init.lua
@@ -1,26 +1,2 @@
--- Set global info here and then require all the extra stuff
-
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.g.have_nerd_font = true
-
-require("config.options")
-
--- [[ Install `lazy.nvim` plugin manager ]]
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system {
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable', -- latest stable release
-        lazypath,
-    }
-end
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup("plugins")
+require("config.lazy")
 require("config.init")
